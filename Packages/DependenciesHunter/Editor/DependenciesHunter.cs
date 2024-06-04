9bdd7e4e14e63e35e87e26456d40fae1858299e7
Per Malmén <per.malmen@nordlight.io>
2024-06-04 14:05:21 +0200

    Added comment

--- Packages/DependenciesHunter/Editor/DependenciesHunter.cs
+++ Packages/DependenciesHunter/Editor/DependenciesHunter.cs
@@ -784,6 +784,7 @@ namespace DependenciesHunter
         private Vector2 _scrollPos = Vector2.zero;
         private Vector2[] _foldoutsScrolls;
 
+        // Added DH to avoid clash with Unity's built in menu item
         [MenuItem("Assets/DH - Find References In Project", false, 20)]
         public static void FindReferences()
         {
